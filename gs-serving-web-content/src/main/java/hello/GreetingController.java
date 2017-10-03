6c1cf81edac081104108bfaf5c4af62a4589a222
helen <zhang_xiaoh@worksap.co.jp>
2017-10-03 11:11:11 +0900

    sample code of gs-serving-web-content

---
+++ gs-serving-web-content/src/main/java/hello/GreetingController.java
@@ -0,0 +1,17 @@
+package hello;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+@Controller
+public class GreetingController {
+	// or access url : http://localhost:8080/ -> go to the index page
+	// access url : http://localhost:8080/greeting or http://localhost:8080/greeting?name=User	
+	@RequestMapping("/greeting")
+	public String greeting(@RequestParam(value="name", required=false, defaultValue="World") String name, Model model) {
+		model.addAttribute("name", name);
+		return "greeting";
+	}
+}
