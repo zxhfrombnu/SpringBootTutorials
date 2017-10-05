811ffc331bc3a6cb32fe5e7a9b9648aef7c5ce38
helen <zhang_xiaoh@worksap.co.jp>
2017-10-05 12:08:14 +0900

    sample code of gs-caching

---
+++ gs-caching/src/main/java/hello/SimpleBookRepository.java
@@ -0,0 +1,23 @@
+package hello;
+
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Component;
+
+@Component
+public class SimpleBookRepository implements BookRepository {
+	@Override
+	@Cacheable("books")
+	public Book getByIsbn(String isbn) {
+		simulateSlowService();
+		return new Book(isbn, "Some book");
+	}
+	
+	private void simulateSlowService() {
+		try {
+			long time = 1000L;
+			Thread.sleep(time);
+		} catch (InterruptedException e) {
+			throw new IllegalStateException(e);
+		}
+	}
+}
