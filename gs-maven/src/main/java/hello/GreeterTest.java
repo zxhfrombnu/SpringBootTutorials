42306640453df67452947222ba98338d0741cac4
helen <zhang_xiaoh@worksap.co.jp>
2017-10-05 13:01:47 +0900

    sample of gs-maven

---
+++ gs-maven/src/main/java/hello/GreeterTest.java
@@ -0,0 +1,12 @@
+package hello;
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.junit.Assert.*;
+import org.junit.Test;
+
+public class GreeterTest {
+	private Greeter greeter = new Greeter();
+	@Test
+	public void greeterSaysHello() {
+		assertThat(greeter.sayHello(), containsString("Hello"));
+	}
+}
